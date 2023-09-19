e614164a66ab862eaeb59798431e508bdb889064
jefferson.avanade <jefferson.magalhaes.avanade@superdigital.com.br>
2023-09-19 10:56:57 -0300

    componentes dinamicos em angular

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
-  styleUrls: ['./big-card.component.css']
+  styleUrls: ['./big-card.component.css'],
 })
 export class BigCardComponent implements OnInit {
+  @Input() photoCover: string = 'https://i.stack.imgur.com/luaq2.png';
+  @Input() cardTitle: string = 'Saiu a nova versão do Angular';
+  @Input() cardDescription: string =
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec aliquam tortor erat, nec rhoncus urna imperdietsed. Phasellus hendrerit velit a dolor condimentum euismod.';
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
