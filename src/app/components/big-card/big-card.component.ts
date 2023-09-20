32ecb390f34ae321d5ed58d06d6df2b7e435e4f4
jefferson.avanade <jefferson.magalhaes.avanade@superdigital.com.br>
2023-09-20 10:53:17 -0300

    content created

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -6,10 +6,10 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./big-card.component.css'],
 })
 export class BigCardComponent implements OnInit {
-  @Input() photoCover: string = 'https://i.stack.imgur.com/luaq2.png';
-  @Input() cardTitle: string = 'Saiu a nova versão do Angular';
-  @Input() cardDescription: string =
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec aliquam tortor erat, nec rhoncus urna imperdietsed. Phasellus hendrerit velit a dolor condimentum euismod.';
+  @Input() photoCover: string = '';
+  @Input() cardTitle: string = '';
+  @Input() cardDescription: string = '';
+  @Input() Id: string = '';
   constructor() {}
 
   ngOnInit(): void {}
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "photo":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/AD72115B9CDAE873B2234BD0908CEF7B45A2D9AD9C66C699239B46E582F70A8B/scale?width=1200&aspectRatio=1.78&format=jpeg",
+    "title":"Homem de ferro em 3D",
+    "description":"Disney anuncia mais um filme da Marvel para 2026 e é um novo filme do novo homem de ferro"
+  },
+  {
+    "id":"2",
+    "photo":"https://versaodublada.com.br/wp-content/uploads/2020/11/series-marvel-disney-plus-1024x576-1.png",
+    "title":"Nova Série em Dezembro de 2025",
+    "description":"Nova Série anunciada para Dezembro de 2025 no Disneu + na nova fase da Marvel"
+  },
+  {
+    "id":"3",
+    "photo":"https://geeknation.net.br/wp-content/uploads/2023/07/imagem_2023-07-21_154020936.png",
+    "title":"The Marvels é a maior expectativa nos cinemas para 2023",
+    "description":"O filme estreia mês que vem nos cinemas e a expectativa do retorno são altas."
+  },
+  {
+    "id":"4",
+    "photo":"https://s2-techtudo.glbimg.com/K-gLzu8Qbm62gNdtbPifiOCqqYI=/0x0:2000x1125/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/A/j/cpRFSdQVaYwQwpe8nZ1w/x-men-o-filme.jpg",
+    "title":"Novo filme do X-MEN",
+    "description":"Marvel revela que está produzindo novo filme do X-men que trará todo o universo para dentro do MCU"
+  }
+]
